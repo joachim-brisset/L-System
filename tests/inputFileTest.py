a6b4a69599443884796ed9bde5559fbb8ef9f2ac
Brisset Joachim <joachim.brisset@eisti.fr>
2021-01-06 17:05:06 +0100

    Adding example for testing

---
+++ tests/inputFileTest.py
@@ -0,0 +1,9 @@
+import l-system as lsys
+import unittest
+
+class fileInputTest(unittest.TestCase):
+    def test_name():
+        self.assertTrue()
+
+if __name__ == "__main__":
+    unittest.main()

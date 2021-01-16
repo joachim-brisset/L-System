1bc480cc45f979decb80527cbf624d26c78dbe04
Brisset Joachim <joachim.brisset@eisti.fr>
2021-01-16 19:59:14 +0100

    Fix bug

--- tests/inputFileTest.py
+++ tests/inputFileTest.py
@@ -1,9 +1,17 @@
-import l-system as lsys
+import sys, os
+sys.path.insert(1, os.path.join(sys.path[0], '..'))
+lsys = __import__("l-system") # 'import l-system as lsys' not working because of the dash
+
 import unittest
 
+nextinput = ''
+def input(): # redefine input function to handle user input
+    return nextinput
+
 class fileInputTest(unittest.TestCase):
-    def test_name():
-        self.assertTrue()
+    def test_defaultFile(self):
+        ''' check if existing file is correctly returned '''
+        self.assertTrue(lsys.inputFile(defaultFile=), os.getcwd() + "inputFileTest.py")
 
 if __name__ == "__main__":
-    unittest.main()
+    unittest.main(verbosity=2)
